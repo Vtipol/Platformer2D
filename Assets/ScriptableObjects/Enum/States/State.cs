3fcea0cba53b8d5962cb66d03d4474a9f105d427
felipc273 <felipc273@gmail.com>
2025-02-11 17:50:11 +0100

    Add
    + Added StateEvent container and StateDrivenEvents
    + Added basic ScriptableVars

---
+++ Assets/ScriptableObjects/Enum/States/State.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(State), menuName = nameof(ScriptableObject) + "/"
+    + nameof(State)
+    )]
+
+public class State : ScriptableVar { }
